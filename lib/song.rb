8a7c382f8094bba5efa57cdc512e5a929c36b6dc
clifford <clifford@noukod.com>
2018-10-08 19:52:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,19 @@
-class MP3Importer
-  attr_accessor :path
+class Song
+  attr_accessor :artist, :name
 
-  def initialize(path)
-    @path = path
+  def initialize(name)
+    @name = name
   end
 
-  def files
-    files = []
-    Dir.new(self.path).each do |file|
-      files << file if file.length > 4
-    end
-    files
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
 
-  def import
-    self.files.each do |filename|
-      Song.new_by_filename(filename)
-    end
+  def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
+    song
   end
 end
